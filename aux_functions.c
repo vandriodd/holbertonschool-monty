8b17f1d6f7ae3a0015a0d5f5ead28cde61083742
vandriodd <ludasaadi@gmail.com>
2023-05-09 17:42:35 -0700

    Aux function for gets opcode added

--- aux_functions.c
+++ aux_functions.c
@@ -1,1 +1,28 @@
 #include "monty.h"
+
+/**
+ * get_func - chooses the correct function based on the input
+ * @op: opcode
+ *
+ * Return: the function corresponding to the opcode,
+ * 0 otherwise
+ */
+int (*get_func(char *op))(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}};
+
+	while (i < 2)
+	{
+		if (!strcmp(op, opcodes[i].opcode))
+		{
+			return (opcodes[i].f);
+		}
+		i++;
+	}
+	return (0);
+}
