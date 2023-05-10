c54e430f2b8510700e7ba73df4aaad77c32bd5a3
vandriodd <ludasaadi@gmail.com>
2023-05-09 17:43:28 -0700

    Update header file

--- monty.h
+++ monty.h
@@ -46,6 +46,8 @@ typedef struct instruction_s
 #define DELIM " \n\t"
 
 /* Protoypes */
-/* Placeholder */
+int (*get_func(char *op))(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 
 #endif
