ca91208f60e81720e86a7c0de38aff40d5050cfb
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-16 05:53:54 -0700

    Sub prototype

--- monty.h
+++ monty.h
@@ -58,4 +58,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
+/* Advanced */
+void sub(stack_t **stack, unsigned int line_number);
+
 #endif
