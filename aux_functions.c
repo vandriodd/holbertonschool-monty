084b20740a0d67aa8f4df84bdcdfb98f71198fcb
vandriodd <ludasaadi@gmail.com>
2023-05-15 19:27:42 -0700

    Fix Betty

--- aux_functions.c
+++ aux_functions.c
@@ -3,31 +3,33 @@
 /**
  * op_tofunc - chooses the correct function based on the opcode
  * @op: opcode
+ *
+ * Return: function attached to opcode
  */
 void (*op_tofunc(char *op))(stack_t **stack, unsigned int line_number)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    instruction_t opcodes[] = {
-        {"push", push},
-        {"pall", pall},
-        /* {"pint", pint},
-        {"pop", pop},
-        {"swap", swap},
-        {"add", add},
-        {"nop", nop}, */
-        {NULL, NULL}
-        };
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+		};
 
-    while (opcodes[i].opcode) /* i < 2 */
-    {
-        if (strcmp(opcodes[i].opcode, op) == 0)
-        {
-            return (opcodes[i].f);
-        }
-        i++;
-    }
-    return (NULL);
+	while (opcodes[i].opcode) /* i < 7 */
+	{
+		if (strcmp(opcodes[i].opcode, op) == 0)
+		{
+			return (opcodes[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 /**
@@ -36,14 +38,14 @@ void (*op_tofunc(char *op))(stack_t **stack, unsigned int line_number)
  */
 void free_stack(stack_t *stack)
 {
-    stack_t *tmp = NULL;
+	stack_t *tmp = NULL;
 
-    while (stack)
-    {
-        tmp = stack; /* points to current element */
-        stack = stack->next; /* moves */
-        free(tmp);
-    }
+	while (stack)
+	{
+		tmp = stack; /* points to current element */
+		stack = stack->next; /* moves */
+		free(tmp);
+	}
 }
 
 /**
@@ -54,16 +56,16 @@ void free_stack(stack_t *stack)
  */
 int _isdigit(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (str[0] == '-')
-        str[0] = '0';
+	if (str[0] == '-')
+		str[0] = '0';
 
-    while (str[i])
-    {
-        if (isdigit(str[i]) == 0)
-            return (0);
-        i++;
-    }
-    return (1); /* valid integer */
+	while (str[i])
+	{
+		if (isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1); /* valid integer */
 }
