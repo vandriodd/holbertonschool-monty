2a14bb7d7a0d2c82d912d867d1bc703aa98c1414
vandriodd <ludasaadi@gmail.com>
2023-05-15 19:45:40 -0700

    Fix Betty

--- main.c
+++ main.c
@@ -10,43 +10,43 @@
  */
 int main(int ac, char **av)
 {
-    FILE *fd = NULL;
-    char *buff = NULL, *op = NULL;
-    stack_t *stack = NULL;
-    size_t size = 0, line_number = 0;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	FILE *fd = NULL;
+	char *buff = NULL, *op = NULL;
+	stack_t *stack = NULL;
+	size_t size = 0, line_number = 0;
+	void (*f)(stack_t **stack, unsigned int line_number);
 
-    if (ac != 2) /* case of failure: few arguments */
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (ac != 2) /* case of failure: few arguments */
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    fd = fopen(av[1], "r");
-    if (!fd) /* case of failure: open fails */
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        exit(EXIT_FAILURE);
-    }
+	fd = fopen(av[1], "r");
+	if (!fd) /* case of failure: open fails */
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (getline(&buff, &size, fd) != -1)
-    {
-        line_number++;
-        op = strtok(buff, DELIM);
-        if (!op)
-            continue;
+	while (getline(&buff, &size, fd) != -1)
+	{
+		line_number++;
+		op = strtok(buff, DELIM);
+		if (!op)
+			continue;
 
-        f = op_tofunc(op); /* takes the opcode and calls the function attached */
-        if (!f)
-        {
-            fprintf(stderr, "L%lu unknown instruction %s\n", line_number, op);
-            exit(EXIT_FAILURE);
-        }
-        /* else */
-        f(&stack, line_number);
-    }
-    fclose(fd);
-    free(buff);
-    free_stack(stack);
-    return (0);
+		f = op_tofunc(op); /* takes the opcode and calls the function attached */
+		if (!f)
+		{
+			fprintf(stderr, "L%lu unknown instruction %s\n", line_number, op);
+			exit(EXIT_FAILURE);
+		}
+		/* else */
+		f(&stack, line_number);
+	}
+	fclose(fd);
+	free(buff);
+	free_stack(stack);
+	return (0);
 }
